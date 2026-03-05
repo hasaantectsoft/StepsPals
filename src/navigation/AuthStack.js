ee92d2bc058992f1cbce111217d1f9e3aa098245
Muhummad Hasaan <muhammadhasaanwork@gmail.com>
2026-03-05 10:20:35 +0500

    unauth

--- src/navigation/AuthStack.js
+++ src/navigation/AuthStack.js
@@ -1,15 +1,26 @@
 import React from 'react';
-
+import Home from '../screens/AuthStack/Home/Home';
+import LeaderBoard from '../screens/AuthStack/LeaderBoard/LeaderBoard';
+import Settings from '../screens/AuthStack/Settings/Settings';
+import tutorial from '../screens/AuthStack/Tutorials/Tutorials';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import GraveYard from '../screens/AuthStack/GraveYard/GraveYard';
 
 function AuthStack() {
   const Stack = createNativeStackNavigator();
+  const isnewaccount = true;
+  const screens = {
+    tutorial: tutorial,
+    Home:Home,
+    GraveYard: GraveYard,
+    LeaderBoard: LeaderBoard,
+    Settings: Settings
 
-  const screens = {};
+  };
 
   return (
     <Stack.Navigator
-      initialRouteName=""
+      initialRouteName={isnewaccount ? 'tutorial' : 'Home'}
       screenOptions={{
         headerShown: false,
         statusBarAnimation: 'fade',
