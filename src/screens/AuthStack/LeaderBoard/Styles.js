15895c4c7238afcb2b61b6bcebea4bc6c38980f4
Muhummad Hasaan <m.hasaan@tectsoft.com>
2026-03-19 13:53:14 +0500

    Update Styles.js

--- src/screens/AuthStack/LeaderBoard/Styles.js
+++ src/screens/AuthStack/LeaderBoard/Styles.js
@@ -7,6 +7,7 @@ export const styles = StyleSheet.create({
     flex: 1,
     paddingHorizontal: moderateScale(10),
     paddingTop: moderateScale(50),
+    paddingBottom: moderateScale(120)
 
 
   },
